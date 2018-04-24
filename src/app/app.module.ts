dfbd7279d8f97747d911a4de291c5ad92565c62b
Boss <bossyasbt@gmail.com>
2018-04-24 17:08:27 +0700

    New firebase 4.12.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,43 +11,17 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-// Import library firbease
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase } from 'angularfire2/database';
-
-import { UserProvider } from '../providers/user/user';
-import { AuthProvider } from '../providers/auth/auth';
-
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { LoginPage } from '../pages/login/login';
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyANoo4ZrYC6sLzlh-boG58rSoO85j6PKLs",
-  authDomain: "event-rsu-app.firebaseapp.com",
-  databaseURL: "https://event-rsu-app.firebaseio.com",
-  projectId: "event-rsu-app",
-  storageBucket: "event-rsu-app.appspot.com",
-  messagingSenderId: "828624527899"
-};
-
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    LoginPage
-
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFirestoreModule.enablePersistence(),
-    AngularFireStorageModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -55,17 +29,12 @@ export const firebaseConfig = {
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    LoginPage
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthProvider,
-    UserProvider,
-    AngularFireAuth,
-    AngularFireDatabase,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,14 @@
-import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
-//import { AuthProvider } from "../../providers/auth/auth";
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
 @Component({
-  selector: "page-home",
-  templateUrl: "home.html"
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(
-    public navCtrl: NavController,
-    //public authProvider: AuthProvider
-  ) {}
 
-  // logout() {
-  // 	this.authProvider.logout();
-  // 	this.navCtrl.setRoot("LoginPage");
-  // }
+  constructor(public navCtrl: NavController) {
+
+  }
+
 }
